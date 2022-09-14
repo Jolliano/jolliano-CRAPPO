ddc522f94972058b138ec9abbd36ac604f3e7005
Jolliano <jolliano@ymail.com>
2022-09-14 13:43:19 +0100

    Adjusted text sizes

--- src/App.svelte
+++ src/App.svelte
@@ -160,7 +160,7 @@ let ethhover, ltchover
         Revenue will change based on mining difficulty and Ethereum price.
        </p>
     </div>
-    <div class="lg:w-10/12 w-full mx-auto lg:pt-96 pt-[36rem] pb-12 text-black text-center lg:text-6xl text-4xl leading-relaxed">
+    <div class="lg:w-10/12 w-full mx-auto lg:pt-96 pt-[36rem] pb-12 text-black text-center lg:text-6xl text-3xl leading-relaxed">
         <p class="py-12 lg:w-8/12 w-full mx-auto font-bold">Trade securely and market the high growth cryptocurrencies.</p>
         <div class="flex justify-between lg:flex-row flex-col">
             <div class="basis-[30%] place-items-center grid py-12 rounded-3xl mb-12 lg:mb-0 px-6 lg:mx-0 mx-6" class:btchover>
